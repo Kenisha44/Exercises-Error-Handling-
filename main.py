4b52fe4ce2432b985a4665d362bf61ccfe19060d
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-07-07 17:16:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Error Handling 
+
+while True: 
+    try: 
+        age = int(input('what is your age?'))
+        10/age
+    except ValueError: 
+        print('please enter a number')
+    except ZeroDivisionError: 
+        print('please enter age higher than 0')
+    else:
+        print('thank you!')
+        break 
+    finally: 
+        print('ok, I am finally done')
